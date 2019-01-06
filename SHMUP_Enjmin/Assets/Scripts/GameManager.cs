e7618d5840c779208e7ed3431486b81df4616a10
Bomb-X <bombix34@gmail.com>
2019-01-06 19:47:08 +0100

    Ecran game over place holder ( scène MainScene )

---
+++ SHMUP_Enjmin/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	int score=0;
+
+	[SerializeField]
+	GameObject gameOverUI;
+
+	GameObject player;
+
+	void Start () 
+	{
+		player=GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	public void AddScore()
+	{
+		score++;
+	}
+
+	public void GameOver()
+	{
+		GameObject gameover = Instantiate(gameOverUI, transform.position,Quaternion.identity) as GameObject;
+		gameover.GetComponent<GameOverUI>().scoreText.text=score.ToString();
+		player.GetComponent<PlayerManager>().Die();
+		gameover.SetActive(true);
+	}
+
+	public void RelaunchGame()
+	{
+		SceneManager.LoadScene("MainScene");
+	}
+
+//SINGLETON________________________________________________________________________________________________
+
+	private static GameManager s_Instance = null;
+
+    // This defines a static instance property that attempts to find the manager object in the scene and
+    // returns it to the caller.
+    public static GameManager instance
+    {
+        get
+        {
+            if (s_Instance == null)
+            {
+                // This is where the magic happens.
+                //  FindObjectOfType(...) returns the first AManager object in the scene.
+                s_Instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+            }
+
+            // If it is still null, create a new instance
+            if (s_Instance == null)
+            {
+                Debug.Log("error");
+                GameObject obj = new GameObject("Error");
+                s_Instance = obj.AddComponent(typeof(GameManager)) as GameManager;
+            }
+
+            return s_Instance;
+        }
+    }
+}
