b524658310854f4a501db90a5ee1a6997f977f08
Unknown <razeltif@gmail.com>
2019-01-06 20:27:09 +0100

    ajout patternsdéplacements des potes (pas encore désac qd entré bulle)

---
+++ SHMUP_Enjmin/Assets/Scripts/Pattern/UpDownPattern.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownPattern : PatternInterface {
+
+    // direction initiale du pote
+    public bool directionUp = true;
+    // distance parcouru (entre la position la plus haute et la position la plus basse)
+    public float distance = 3.0f;
+    // vitesse de déplacement du pote
+    public float speed = 0.1f;
+
+    private float distanceDone = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Update()
+    {
+
+        distanceDone += Time.deltaTime * speed;
+        if (distanceDone > distance)
+        {
+            directionUp = !directionUp;
+            distanceDone = 0;
+        }
+        if (directionUp)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime * speed, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - Time.deltaTime * speed, transform.position.z);
+        }
+    }
+}
