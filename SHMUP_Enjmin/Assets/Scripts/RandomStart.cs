fed54f0355443736ff0aa7457e7b41b8dac337a1
Amaury H <rhadamanthe@caramail.com>
2019-01-18 16:17:22 +0100

    Animation Kraken Fond (creation assets + animation + aléatoire)

---
+++ SHMUP_Enjmin/Assets/Scripts/RandomStart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStart : MonoBehaviour {
+
+        Animator anim;
+
+    float chrono;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+        chrono = Random.Range(3f, 8f);
+    }
+
+    void Update()
+    {
+        if (chrono > 0)
+            chrono -= Time.deltaTime;
+        else
+        {
+            anim.SetTrigger("launch");
+            chrono = Random.Range(3f, 8f);
+        }
+    }
+
+}
